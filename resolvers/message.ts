b1c4520aa78e1c2c88040646fb4410e378b2d036
Chimson <crunor55@hotmail.com>
2021-03-21 12:10:13 +0100

    fixes

--- resolvers/message.ts
+++ resolvers/message.ts
@@ -6,7 +6,15 @@ import Message from "../entity/Message";
 import { RedisPubSub } from "graphql-redis-subscriptions";
 import { withFilter } from "graphql-subscriptions";
 import Redis from "ioredis";
-const redis = new Redis({
+
+const redisOne = new Redis({
+  host: `${process.env.REDIS_HOST}`,
+  port: 18964,
+  password: process.env.REDIS_PASSWORD,
+  username: process.env.REDIS_USERNAME,
+  retryStrategy: (options: any) => Math.max(options.attempt * 100, 3000),
+});
+const redisTwo = new Redis({
   host: `${process.env.REDIS_HOST}`,
   port: 18964,
   password: process.env.REDIS_PASSWORD,
@@ -14,8 +22,8 @@ const redis = new Redis({
   retryStrategy: (options: any) => Math.max(options.attempt * 100, 3000),
 });
 const redisPubSub = new RedisPubSub({
-  subscriber: redis,
-  publisher: redis,
+  subscriber: redisOne,
+  publisher: redisTwo,
 });
 
 export default {
